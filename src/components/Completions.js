c34816630f04889536ea50ec8a29c3cb6baea11c
Luke Jackson <jacksonluke96@yahoo.co.uk>
2017-04-03 19:06:51 +0100

    Task completion bar chart
    A bar chart now displays the distribution of effort by each house member

--- src/components/Completions.js
+++ src/components/Completions.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ObjectTable from './ObjectTable';
 import CompletionRow from './CompletionRow';
+import { BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar } from 'recharts';
 let tools = require('../clientTools');
 
 
@@ -15,26 +16,43 @@ class HC_members extends React.Component {
         super(props);
 
         this.state = {
+            tasks: this.props.tasks
         };
-
     }
 
     componentDidMount() {
 
+        /*
+        */
     }
 
     render() {
-
         let errorMessage;
 
         if (this.state.error) {
             errorMessage = this.state.error;
         } else
             errorMessage = '';
-        
+
         let completionRows = (this.props.tasks === null) ? (<tr><td>Loading</td></tr>) : this.props.tasks.map((completion) => {
             return (<CompletionRow completion={completion} key={completion._id} />);
         });
+
+        let barData = [];
+        if (this.props.tasks) {
+            for (let i = 0; i < this.props.tasks.length; i++) {
+                let found = false;
+                for (let b = 0; b < barData.length; b++) {
+                    if (barData[b].id === this.props.tasks[i].userId){
+                        found = true;
+                        barData[b].score = barData[b].score + this.props.tasks[i].difficulty;
+                    }
+                }
+                if (!found){
+                    barData.push({id: this.props.tasks[i].userId, name: this.props.tasks[i].fname, score: this.props.tasks[i].difficulty});
+                }
+            }
+        }
 
         return (
             <div>
@@ -52,6 +70,14 @@ class HC_members extends React.Component {
                 <label>
                     <div>{errorMessage}</div>
                 </label>
+                <BarChart width={730} height={250} data={barData}>
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="score" fill="#228B22" />
+                </BarChart>
             </div>);
     }
 }
