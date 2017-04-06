7582974d732c43d98ff917a743701a20d50eadf7
Luke Jackson <jacksonluke96@yahoo.co.uk>
2017-04-06 16:03:16 +0100

    Improved clarity of selected tab
    Disabled tabs not available during first house creation and added colour

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -18,7 +18,11 @@ class AppShell extends React.Component {
       page: null,
       completions: null,
       error: false,
-      icon: false
+      icon: false,
+      b_t: '',
+      b_h: '',
+      b_c: '',
+      b_a: ''
     }
 
     this.componentDidMount = this.componentDidMount.bind(this);
@@ -33,12 +37,10 @@ class AppShell extends React.Component {
 
     //Get user information
     tools.get('/json/user', this, function (data, stateRef) {
-
       if (typeof (Storage) !== "undefined") {
         let houseSelectionJSON = localStorage.getItem('houseSelection');
         if (houseSelectionJSON) {
           let houseSelection = JSON.parse(houseSelectionJSON);
-
           if (houseSelection.userId === data.id)
             stateRef.setState({
               currentHouse: houseSelection.name,
@@ -59,31 +61,29 @@ class AppShell extends React.Component {
     //Get all of the houses the user is a member of
     tools.get('/json/houses', this, function (data, stateRef) {
       stateRef.setState((prevState, props) => {
-        if (prevState.currentHouse && prevState.currentHouseId)
+        if (prevState.currentHouse && prevState.currentHouseId) 
+          return {houses: data.houses}
+        else 
           return {
             houses: data.houses,
-            page: (data.houses.length > 0) ? 'housestats' : 'create',
-          }
-        else
-          return {
-            houses: data.houses,
-            page: (data.houses.length > 0) ? 'housestats' : 'create',
             currentHouse: (data.houses.length > 0) ? data.houses[0].name : null,
             currentHouseId: (data.houses.length > 0) ? data.houses[0]._id : null
           }
       });
-      stateRef.pullData();
+      stateRef.pullData(data);
     });
-
   }
 
-  pullData() {
+  pullData(houseData) {
     //Get all the tasks for the selected house
     tools.get('/json/tasks/' + this.state.currentHouse, this, (data, stateRef) => {
       stateRef.setState({ tasks: data });
     });
     tools.get('/json/completions/' + this.state.currentHouse, this, (data, stateRef) => {
-      stateRef.setState({ completions: data });
+      stateRef.setState({ completions: data }, () => {
+        //Now we have all the data, we can decide whether to show the stats or house creation interface (wrapping for event function)
+        stateRef.pageChange({ target: { value: (houseData.houses.length > 0) ? 'housestats' : 'create' }, preventDefault: () => { } });
+      });
     });
     tools.get('/json/icon/' + this.state.currentHouse, this, (data, stateRef) => {
       stateRef.setState({ icon: data.icon });
@@ -127,32 +127,42 @@ class AppShell extends React.Component {
   pageChange(e) {
     e.preventDefault();
     this.setState({ page: e.target.value });
+    switch (e.target.value) {
+      case 'create':
+        this.setState({
+          content: <HouseCreate />,
+          b_t: '', b_c: 'activeTab', b_h: '', b_a: ''
+        });
+        break;
+      case 'complete':
+        this.setState({
+          content: <TaskCompletion refresh={this.componentDidMount} house={this.state.currentHouse} houseId={this.state.currentHouseId} tasks={this.state.tasks} />,
+          b_t: 'activeTab', b_c: '', b_h: '', b_a: ''
+        });
+        break;
+      case 'housestats':
+        this.setState({
+          content: <Completions tasks={this.state.completions} houseName={this.state.currentHouse} />,
+          b_t: '', b_c: '', b_h: 'activeTab', b_a: ''
+        });
+        break;
+      case 'admin':
+        this.setState({
+          content: <Admin refresh={this.componentDidMount} house={this.state.currentHouse} houseId={this.state.currentHouseId} tasks={this.state.tasks} completions={this.state.completions} delete={(id, url) => { this.delete(id, url) }} />,
+          b_t: '', b_c: '', b_h: '', b_a: 'activeTab'
+        });
+        break;
+      default:
+        this.setState({ content: (<p>Waiting for content...</p>) });
+        break;
+    }
   }
 
   render() {
     let housesList = this.state.houses.map((house) => <option key={house.name} value={JSON.stringify([house.name, house._id])}>{house.name}</option>);
     let hasHouses = (this.state.houses.length != 0);
 
-    let content;
-    switch (this.state.page) {
-      case 'create':
-        content = <HouseCreate />;
-        break;
-      case 'complete':
-        content = <TaskCompletion refresh={this.componentDidMount} house={this.state.currentHouse} houseId={this.state.currentHouseId} tasks={this.state.tasks} />;
-        break;
-      case 'housestats':
-        content = <Completions tasks={this.state.completions} houseName={this.state.currentHouse} />;
-        break;
-      case 'admin':
-        content = <Admin refresh={this.componentDidMount} house={this.state.currentHouse} houseId={this.state.currentHouseId} tasks={this.state.tasks} completions={this.state.completions} delete={(id, url) => { this.delete(id, url) } } />;
-        break;
-      default:
-        content = (<p>Waiting for content...</p>);
-        break;
-    }
-
-    let icon = <div></div>
+    let icon = <div></div>;
     /*
         if (this.state.icon) {
           var canvas = document.createElement('canvas');
@@ -171,22 +181,22 @@ class AppShell extends React.Component {
         <a href='/login'>Log in to view this page.</a> :
         <div className='container'>
           <div className='row'>
-            <button className='three columns' onClick={this.pageChange} value={'complete'}>
+            <button className={'three columns ' + this.state.b_t} onClick={hasHouses ? this.pageChange : ()=>{}} value={'complete'}>
               Task+
             </button>
 
-            <button className='three columns' onClick={this.pageChange} value={'housestats'}>
+            <button className={'three columns ' + this.state.b_h} onClick={hasHouses ? this.pageChange : ()=>{}} value={'housestats'}>
               House Stats
                 </button>
-            <button className='three columns' onClick={this.pageChange} value={'create'}>
+            <button className={'three columns ' + this.state.b_c} onClick={this.pageChange} value={'create'}>
               Create
                 </button>
-            <button className='three columns' onClick={this.pageChange} value={'admin'}>
+            <button className={'three columns ' + this.state.b_a} onClick={hasHouses ? this.pageChange : ()=>{}} value={'admin'}>
               Admin
                 </button>
           </div>
           <div className='innerComponent'>
-            {content}
+            {this.state.content}
           </div>
           <div className='row'>
             <h5 className='two columns'>
